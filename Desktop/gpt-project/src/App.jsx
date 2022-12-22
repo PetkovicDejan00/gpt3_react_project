30fe5aff28594390d6b4beafc9027b1335aa8001
Petkovic <dexxavr@hotmail.com>
2022-12-22 15:20:42 +0100

    first-commit

---
+++ Desktop/gpt-project/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Header, Blog, Possibility, Features, WhatGPT3, Footer} from './containers'
+import {Brand, CTA, Navbar} from './components'
+import './App.css'
+function App() {
+
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
